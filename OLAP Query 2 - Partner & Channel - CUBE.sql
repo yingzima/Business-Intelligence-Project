4338b98add171f0c6d44b2efa70d80c01df033a1
Yingzi <32115581+yingzima@users.noreply.github.com>
2019-01-06 23:18:32 -0500

    Update OLAP Query 2 - Partner & Channel - CUBE.sql

--- OLAP Query 2 - Partner & Channel - CUBE.sql
+++ OLAP Query 2 - Partner & Channel - CUBE.sql
@@ -1,30 +1,37 @@
-SELECT
-partner, 
-channel,
-count(*) as Total,
-SUM(converted) AS Converted,
-SUM(converted)/COUNT(*) AS ConversionRate,
-AVG(raw_score) as AvgRawScore
-FROM lead_view
-GROUP BY partner, channel
+/*Note - If implemented in OLAP enabled environment, the below queries would use ROLLUP or CUBE 
+functions rather than unioned queries with different group by conditions.*/
+
+    SELECT
+          partner, 
+          channel,
+          count(*) as Total,
+          SUM(converted) AS Converted,
+          SUM(converted)/COUNT(*) AS ConversionRate,
+          AVG(raw_score) as AvgRawScore
+    FROM lead_view
+    GROUP BY partner, channel
+
 UNION
-SELECT 
-partner,
-"" AS channel, 
-count(*) as Total,
-SUM(converted) AS Converted, 
-SUM(converted)/COUNT(*) AS ConversionRate,
-AVG(raw_score) as AvgRawScore
-FROM lead_view
-GROUP BY partner
+
+    SELECT 
+          partner,
+          "" AS channel, 
+          count(*) as Total,
+          SUM(converted) AS Converted, 
+          SUM(converted)/COUNT(*) AS ConversionRate,
+          AVG(raw_score) as AvgRawScore
+    FROM lead_view
+    GROUP BY partner
+
 UNION
-SELECT 
-"" as partner,
-channel, 
-count(*) as Total,
-SUM(converted) AS Converted, 
-SUM(converted)/COUNT(*) AS ConversionRate,
-AVG(raw_score) as AvgRawScore
-FROM lead_view
-GROUP BY channel
-;
+
+    SELECT 
+          "" as partner,
+          channel, 
+          count(*) as Total,
+          SUM(converted) AS Converted, 
+          SUM(converted)/COUNT(*) AS ConversionRate,
+          AVG(raw_score) as AvgRawScore
+    FROM lead_view
+    GROUP BY channel
+    ;
